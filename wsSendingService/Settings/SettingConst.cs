064061f25d1bc7d911c10d63c9702ecab72b7a43
MD Saiful Islam <saifulprogrammer@gmail.com>
2024-05-25 23:16:25 +0600

    modify

---
+++ wsSendingService/Settings/SettingConst.cs
@@ -0,0 +1,12 @@
+﻿
+namespace wsSendingService.Settings
+{
+    public static class SettingConst
+    {
+        public const string TwilioWhatsAppSid = "Twilio.WhatsApp.AccountSid";
+        public const string TwilioWhatsAppToken = "Twilio.WhatsApp.Token";
+
+        public const string TwilioWhatsAppMessagingServiceSid = "Twilio.WhatsApp.MessagingServiceSid";
+        public const string TwilioWhatsAppFileAttachmentSid = "Twilio.WhatsApp.FileAttachmentSid";
+    }
+}
